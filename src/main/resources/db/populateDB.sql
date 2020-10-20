33a87721780cab3e1e3e3696ccc3862e82cda872
Ilya <kineev.ilya@gmail.com>
2020-10-20 19:20:56 +0300

    add meals and meals SQL

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,13 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-06-30 10:00:00', 'Завтрак', 500, 100000),
+       ('2020-06-30 13:00:00', 'Обед', 1000, 100000),
+       ('2020-06-30 20:00:00', 'Ужин', 500, 100000),
+       ('2020-06-01 10:00:00', 'Завтрак', 500, 100000),
+       ('2020-06-01 13:00:00', 'Обед', 1000, 100000),
+       ('2020-06-01 20:00:00', 'Ужин', 510, 100000),
+       ('2020-07-02 14:00:00', 'Обед', 510, 100001),
+       ('2020-07-02 21:00:00', 'Ужин', 1500, 100001);
